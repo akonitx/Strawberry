fe588f2f791329e1cb88d323c7a9569e7badeeda
Sheenaz <taraschorny10@gmail.com>
2023-03-14 17:26:48 +0200

    commit

--- foo.py
+++ foo.py
@@ -1,10 +1,1 @@
-class Car:
-    def __init__(self, car_name) -> None:
-        self.carname = car_name
-
-    def __str__(self) -> str:
-        return self.carname
-
-
-suzuki = Car("suzuki")
-print(suzuki.__str__())
+import django
